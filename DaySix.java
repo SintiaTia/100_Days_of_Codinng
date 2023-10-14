8235465ce0fd7a24647d830f15a44de1c68fa61a
SintiaTia <147256321+SintiaTia@users.noreply.github.com>
2023-10-14 21:55:38 +0800

    Day 6
    hari ini saya membuat program tentang pertukaran antara nilai a dan b.

---
+++ DaySix.java
@@ -0,0 +1,15 @@
+package Days;
+
+public class DaySix {
+    public static void main(String[] args) {
+        int a = 50;
+        int b = 90;
+        int c = a;
+        
+        a = b;
+        b = c;
+        
+        System.out.println("Nilai b: "+a);
+        System.out.println("Nilai a: "+b);
+    }
+}
